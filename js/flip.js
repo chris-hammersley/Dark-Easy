06ba106431d69a46967614b99b3b54871b5b3240
Chris Hammersley <chris-hammersley@users.noreply.github.com>
2024-03-22 13:22:50 -0700

    Magic Shoppe new front/back tarot cards

--- js/flip.js
+++ js/flip.js
@@ -1,13 +1,13 @@
 HTMLElement.prototype.swapClasses = function (dosclasses) {
     var clases = dosclasses.split(/\s*\,\s*/);
-    var entra = clases[0];
-    var sale = clases[1];
-    if (this.classList.contains(sale)) {
-        this.classList.remove(sale);
-        this.classList.add(entra);
+    var start = clases[0];
+    var exit = clases[1];
+    if (this.classList.contains(exit)) {
+        this.classList.remove(exit);
+        this.classList.add(start);
     } else {
-        this.classList.remove(entra);
-        this.classList.add(sale);
+        this.classList.remove(start);
+        this.classList.add(exit);
     }
     return this;
 };
